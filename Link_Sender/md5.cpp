4b4cc470c8c8d71ed98a6b6107304ebfc8414b68
terminatorannihilator <shvv733kn@gmail.com>
2013-12-01 22:17:19 +0200

    Create md5.cpp

---
+++ Link_Sender/md5.cpp
@@ -0,0 +1,11 @@
+#include "md5.h"
+int main() {
+	//оно работает
+    char res[33];
+    char msg[1024];
+  	cin>>msg;
+    //возвращает мд5 msg в res
+    getHash(msg,res);
+    cout<<res;
+    return 0;
+}
