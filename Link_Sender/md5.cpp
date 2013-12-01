d8b6315370147e83eb2991f51a1f4bcff269b36f
terminatorannihilator <shvv733kn@gmail.com>
2013-12-01 22:18:46 +0200

    Update md5.cpp

--- Link_Sender/md5.cpp
+++ Link_Sender/md5.cpp
@@ -2,10 +2,10 @@
 int main() {
 	//оно работает
     char res[33];
-    char msg[1024];
-  	cin>>msg;
-    //возвращает мд5 msg в res
-    getHash(msg,res);
-    cout<<res;
-    return 0;
+	char msg[1024];
+	cin>>msg;
+	//возвращает мд5 msg в res
+	getHash(msg,res);
+	cout<<res;
+	return 0;
 }
